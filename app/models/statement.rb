622897596466915bb2736d1c435e94a37243c1c0
Gregg Cameron <gcameronsc@gmail.com>
2016-01-11 08:28:43 -0500

    Fixed a comment

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,4 +1,4 @@
 class Statement < ActiveRecord::Base
   mount_uploader :name, StatementUploader # Tells rails to use this uploader for this model.
-  validates :name, presence: true # Make sure the owner's name is present.
+  validates :name, presence: true # Make sure the file name is present.
 end
