b4c5813036d81821bfa060dba195728a852e138c
Gregg Cameron <gcameronsc@gmail.com>
2016-01-14 09:50:47 -0500

    Switching to S3 storage complete

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20160112112902) do
   enable_extension "plpgsql"
 
   create_table "statements", force: :cascade do |t|
-    t.string   "name",                    null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.string   "attachment_file_name"
